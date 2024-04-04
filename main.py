565e0808363971fcad1912ec6c023c18d43cea63
Youssef Abdelrahim <54071798+yrehim7@users.noreply.github.com>
2024-04-04 03:16:20 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print ("Welcome to the YouTube Channel Generator:")
+name = input ("What is your nickname? \n")
+channel_activity = input ("What is your channel about? \n")
+print ("You could name your channel " + "(" + channel_activity + " with " + name + ")")
